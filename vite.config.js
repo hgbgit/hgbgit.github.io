587ade12fbabcc6e507a2d760b07d213cf246b57
Hugo Barros <git@hugobarros.com.br>
2026-03-11 14:51:34 -0300

    fix config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/sinestesia/", // Replace with your repo name
+  base: "/", // Replace with your repo name
 });
